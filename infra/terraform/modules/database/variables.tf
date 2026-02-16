fc8e9197357d4c70a32372632673aec51e20b2b1
XeroSWM <wxmonteros@uce.edu.ec>
2026-02-16 11:27:33 -0500

    terraform pruebas

--- infra/terraform/modules/database/variables.tf
+++ infra/terraform/modules/database/variables.tf
@@ -1,5 +1,9 @@
 variable "subnet_ids" { type = list(string) }
 variable "security_group_id" { type = string }
 variable "db_name" { default = "taskmaster_db" }
-variable "db_username" { default = "admin" }
+variable "db_username" {
+  description = "Usuario maestro de la base de datos"
+  type        = string
+  default     = "dbadmin"  # <--- ANTES DECÍA "admin"
+}
 variable "db_password" { sensitive = true }

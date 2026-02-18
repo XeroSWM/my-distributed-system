951971e2b26331e9b50b9387c42441108437aa12
XeroSWM <wxmonteros@uce.edu.ec>
2026-02-18 11:14:50 -0500

    sfsdf

--- infra/terraform/modules/compute/outputs.tf
+++ infra/terraform/modules/compute/outputs.tf
@@ -1,20 +1,19 @@
+# infra/terraform/modules/compute/outputs.tf
+
 output "instance_id" {
-  description = "ID de la instancia EC2"
-  value       = aws_instance.web.id
+  value = aws_instance.web.id
 }
 
 output "public_ip" {
-  description = "IP Pública para acceso desde internet"
-  value       = aws_eip.web_ip.public_ip
+  value = aws_eip.web_ip.public_ip
 }
 
 output "public_dns" {
-  description = "DNS Público de la instancia"
-  value       = aws_eip.web_ip.public_dns
+  value = aws_eip.web_ip.public_dns
 }
 
-# --- ESTE ES EL QUE TE FALTA ---
+# --- ESTA ES LA LÍNEA MÁGICA QUE TE FALTA ---
+# Sin esto, Terraform no puede pasarle la IP al .env
 output "private_ip" {
-  description = "IP Privada para comunicación interna entre servidores"
-  value       = aws_instance.web.private_ip
+  value = aws_instance.web.private_ip
 }
